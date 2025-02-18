2d1b7a47d44f40b2025c1b42e6362fe92e741a54
smallfu6 <smallfu666@gmail.com>
2025-02-18 13:11:02 +0800

    feat: add icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import "@/styles/globals.css";
-// import Head from 'next/head';
+import Head from 'next/head';
 import type { AppProps } from "next/app";
 
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps<{ session: Session }>) {
   return (
-      // <Head>
-      //   <link rel="icon" type="image/png" sizes="32x32" href="https://next-auth.js.org/img/logo/logo-sm.png" />
-      // </Head>
+    <>
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="https://openbuild.xyz/favicon-96x96.png" />
+      </Head>
       <Component {...pageProps} />
+      </>
   )
 }
 
